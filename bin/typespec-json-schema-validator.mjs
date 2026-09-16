368e12b91195b52ed4ac1acf1482203f9969052e
Alexander Mills <alex@oresoftware.com>
2026-09-16 01:09:06 -0500

    feat(config): expose config command on tjsv

--- bin/typespec-json-schema-validator.mjs
+++ bin/typespec-json-schema-validator.mjs
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
 import { main } from '../src/cli.mjs';
+import { runConfigCommand } from '../src/config-cli.mjs';
 
-process.exitCode = await main(process.argv);
+process.exitCode = process.argv[2] === 'config'
+  ? await runConfigCommand(process.argv)
+  : await main(process.argv);
